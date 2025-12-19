1e1f33c0e46fc9da39a8c9505f8a22e8c1822234
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2025-12-19 18:07:34 +0100

    use random delay between 500 and 900ms before sending back 401

--- crates/rest-api/src/middlewares/sleep_unauthorized.rs
+++ crates/rest-api/src/middlewares/sleep_unauthorized.rs
@@ -1,11 +1,13 @@
 use std::time::Duration;
 
 use axum::{extract::Request, http::StatusCode, middleware::Next, response::IntoResponse};
+use rand::Rng;
 
 pub async fn sleep_on_401(request: Request, next: Next) -> impl IntoResponse {
     let res = next.run(request).await;
     if res.status() == StatusCode::UNAUTHORIZED {
-        tokio::time::sleep(Duration::from_secs(1)).await;
+        let delay = Duration::from_millis(rand::rng().random_range(500..=900));
+        tokio::time::sleep(delay).await;
     }
     res
 }
