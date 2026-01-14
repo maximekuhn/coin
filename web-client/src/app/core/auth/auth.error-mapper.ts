91442353a69d786579ce00bc8022e72df0f9b712
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2026-01-14 21:48:35 +0100

    basic setup for angular eslint

--- web-client/src/app/core/auth/auth.error-mapper.ts
+++ web-client/src/app/core/auth/auth.error-mapper.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { AuthError, AuthErrorKind } from "./auth.errors";
 
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export function mapToAuthError(_: HttpErrorResponse): AuthError {
   // TODO: implement mapToAuthError
   return { kind: AuthErrorKind.Unknown };
