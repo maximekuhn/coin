5932c5cb9ef19e01c2a379a7232fde3213f2954d
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2026-01-31 19:13:08 +0100

    use currency pipe in groups overview's balances

--- web-client/src/app/layout/page/home-page/group-overview-list/group-overview-list.ts
+++ web-client/src/app/layout/page/home-page/group-overview-list/group-overview-list.ts
@@ -5,11 +5,11 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { UserOverview } from '../../../../core/group/group.models';
 import { formatUsername } from '../../../../shared/utils/user.utils';
-import { DatePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-group-overview-list',
-  imports: [MatProgressSpinnerModule, MatListModule, MatIconModule, DatePipe],
+  imports: [MatProgressSpinnerModule, MatListModule, MatIconModule, DatePipe, CurrencyPipe],
   templateUrl: './group-overview-list.html',
   styleUrl: './group-overview-list.scss',
 })
